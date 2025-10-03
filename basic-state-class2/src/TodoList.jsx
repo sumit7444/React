eef365b6b4d7a580e7eefce0f3f1a08fc9a6373d
Sumit Prajapati <162676130+sumit7444@users.noreply.github.com>
2025-10-03 09:32:10 +0530

    Todo update

--- basic-state-class2/src/TodoList.jsx
+++ basic-state-class2/src/TodoList.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 export default function TodoList() {
   const [todos, setTodos] = useState([{ task: "sample-task", id: uuidv4() }]);
@@ -16,9 +16,28 @@ export default function TodoList() {
     setNewTodo(event.target.value);
   };
 
-  // Delete function
   const deleteTask = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id));
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  // Uppercase all tasks
+  const upperCaseAll = () => {
+    setTodos(
+      todos.map((todo) => ({
+        ...todo,
+        task: todo.task.toUpperCase(),
+      }))
+    );
+  };
+
+  // Lowercase all tasks
+  const lowerCaseAll = () => {
+    setTodos(
+      todos.map((todo) => ({
+        ...todo,
+        task: todo.task.toLowerCase(),
+      }))
+    );
   };
 
   return (
@@ -34,15 +53,16 @@ export default function TodoList() {
       <hr />
       <h4>Task Todo</h4>
       <ul>
-        {todos.map((todo) => {
-          return (
-            <li key={todo.id}>
-              {todo.task}{" "}
-              <button onClick={() => deleteTask(todo.id)}>Delete</button>
-            </li>
-          );
-        })}
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.task}{" "}
+            <button onClick={() => deleteTask(todo.id)}>Delete</button>
+          </li>
+        ))}
       </ul>
+      <br />
+      <button onClick={upperCaseAll}>Uppercase All</button>{" "}
+      <button onClick={lowerCaseAll}>Lowercase All</button>
     </div>
   );
 }
